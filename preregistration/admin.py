11c66b44b92b13881ca61e63076710ec504ff423
t-josejimenez <t-josejimenez@itesm.mx>
2024-11-26 23:37:15 -0600

    feat: Abstract Class Person

--- preregistration/admin.py
+++ preregistration/admin.py
@@ -7,26 +7,12 @@ from django.utils.safestring import mark_safe
 @admin.register(Preregister)
 class PreregisterAdmin(admin.ModelAdmin):
     list_display = (
-        'folio','photo_preview', 'name'
+        'photo_preview', 'folio','name'
     )
     search_fields = ( 'folio', 'name', 'curp', 'email')
     list_filter = ('gender',)
     ordering = ('folio',)
-    readonly_fields = ('calculate_age', 'photo_preview')
-
-    def calculate_age(self, obj):
-        """Method to calculate the member's age based on their birth date."""
-        if obj.birth_date:
-            today = timezone.now().date()
-            age = today.year - obj.birth_date.year
-            if today.month < obj.birth_date.month or (
-                today.month == obj.birth_date.month and today.day < obj.birth_date.day
-            ):
-                age -= 1
-            return age
-        return _("Not available")
-
-    calculate_age.short_description = _('Age')
+    readonly_fields = ('folio', 'age', 'age_segment', 'photo_preview')
 
     def photo_preview(self, obj):
         """Method to display a photo preview in the admin."""
@@ -36,25 +22,12 @@ class PreregisterAdmin(admin.ModelAdmin):
 
     photo_preview.short_description = _('Photo Preview')
 
-    #def save_model(self, request, obj, form, change):
-    #    # Guarda el objeto Member primero para asignar un ID
-    #    super().save_model(request, obj, form, change)
-#
-    #    # Realiza validaciones relacionadas después de guardar
-    #    primary_contacts = obj.contacts.filter(is_primary=True)
-    #    emergency_contacts = obj.contacts.filter(is_emergency=True)
-#
-    #    if primary_contacts.count() != 1:
-    #        raise ValueError(_("There must be exactly one primary contact."))
-    #    if emergency_contacts.count() != 1:
-    #        raise ValueError(_("There must be exactly one emergency contact."))
-
     # Fieldsets for grouping fields in the admin form
     fieldsets = (
         (_('General Information'), {
             'fields': (
-                'photo_preview','photo', 'member', 'name', 'curp', 'email', 'phone_number',
-                'gender', 'birth_date', 'calculate_age'
+                'photo_preview','photo', 'folio', 'name', 'curp', 'email', 'phone_number',
+                'gender', 'birth_date', 'age', 'age_segment'
             ),
         }),
         (_('Health Conditions'), {
--- preregistration/models.py
+++ preregistration/models.py
@@ -1,54 +1,16 @@
-import re
 import uuid
 from django.db import models
-from django.core.exceptions import ValidationError
-from crm.models import DiscoverySource, Member
+from crm.models import Person, Member, DiscoverySource  
 from django.utils.translation import gettext_lazy as _
 
 
-class Preregister(models.Model):
+class Preregister(Person):
     """Model representing a pre register member of the academy or club with mandatory fields."""
     folio = models.CharField(max_length=100, unique=True, blank=True, editable=False)
-    name = models.CharField(max_length=255, blank=False)
-    curp = models.CharField(max_length=18, unique=True, blank=False)
-    birth_date = models.DateField(blank=False)
-    
-    gender_choices = [
-        ('M', _('Male')),
-        ('F', _('Female')),
-        ('O', _('Other')),
-    ]
-    gender = models.CharField(max_length=1, choices=gender_choices, blank=False)
-    phone_number = models.CharField(max_length=15, blank=False)
-    email = models.EmailField(blank=False)
-    photo = models.ImageField(upload_to='members_photos/', blank=False)
-    how_did_you_hear = models.ForeignKey(DiscoverySource, on_delete=models.SET_NULL, null=True, blank=False, related_name='preregisters')
-    how_did_you_hear_details = models.CharField(max_length=255, blank=True, null=True)
-    
-    # Health-related boolean fields
-    has_illness = models.BooleanField(default=False, blank=False, verbose_name=_("Has any illness"))
-    has_allergy = models.BooleanField(default=False, blank=False, verbose_name=_("Has any allergy"))
-    has_flat_feet = models.BooleanField(default=False, blank=False, verbose_name=_("Has flat feet"))
-    has_heart_conditions = models.BooleanField(default=False, blank=False, verbose_name=_("Has heart conditions"))
-
-    #
     member = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, blank=False, related_name='member')
 
     def __str__(self):
         return self.name
-
-    def clean(self):
-        # Phone number validation (only digits, between 10 and 15 characters)
-        if not re.match(r'^\d{10,15}$', self.phone_number):
-            raise ValidationError(_("The phone number must contain only digits and be between 10 and 15 characters long."))
-
-        # CURP validation (Mexican CURP format)
-        curp_pattern = r'^[A-Z]{4}\d{6}[HM]{1}[A-Z]{5}[A-Z0-9]{2}$'
-        if not re.match(curp_pattern, self.curp):
-            raise ValidationError(_("The CURP must follow a valid format."))
-
-        # Call the parent's clean() method to ensure no other field validations are missed
-        super().clean()
 
     def save(self, *args, **kwargs):
         if not self.folio:  # Generar solo si no existe
